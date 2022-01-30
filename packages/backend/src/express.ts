634d6572d019bf219a99aa5f1cc9d855e751f651
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2022-01-30 07:01:59 -0500

    [backend] JSON persistent DB.

--- packages/backend/src/express.ts
+++ packages/backend/src/express.ts
@@ -1,6 +1,7 @@
 import { IMediaClip, normalizeMediaClip } from "@syncroc/common";
 import * as express from "express";
 import * as cors from "cors";
+import * as fs from "fs";
 const app = express();
 const port = 3000;
 
@@ -9,16 +10,27 @@ app.use(cors({
 }));
 app.use(express.json());
 
-let clips: { [key: string]: IMediaClip } = {};
+let db: { [key: string]: IMediaClip } = {};
+
+function loadDB() {
+    if (!fs.existsSync("./db.json")) return;
+    db = JSON.parse(fs.readFileSync("./db.json").toString());
+}
+loadDB();
+
+function setDB(newDB: { [key: string]: IMediaClip }) {
+    db = newDB;
+    fs.writeFileSync("./db.json", JSON.stringify(newDB));
+}
 
 app.get("/clip/:id", (req, res) => {
     let id: string = req.params.id;
 
     console.log(`[Download] ${id}`)
-    if (clips[id] === undefined) {
+    if (db[id] === undefined) {
         res.send({ "error": "Not Found" });
     } else {
-        res.send(clips[id]);
+        res.send(db[id]);
     }
 });
 
@@ -26,7 +38,10 @@ app.post("/clip/:id", (req, res) => {
     let id: string = req.params.id;
     let clip: IMediaClip = req.body.clip;
 
-    clips[id] = normalizeMediaClip(clip);
+    setDB({
+        ...db,
+        [id]: normalizeMediaClip(clip)
+    });
 
     console.log(`[Upload] ${id}`)
     res.send({ "result": "Success" });
